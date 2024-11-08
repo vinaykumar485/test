3071dcf303e02e49e4f84c1e9c2deb0046dfc6ae
vinaykumar485 <winay8061@gmail.com>
2024-11-08 15:22:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // app.js
 const express = require('express');
 const app = express();
-const port = 3000;
+//const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, Docker and Jenkins!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
 });
